d50f6257725ff2d782ff08b28b664efe0b21e0af
Fandresena <fandresenamisaina@gmail.com>
2026-04-27 11:18:27 +0300

    binaire

--- devoir10.c
+++ devoir10.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
 void binaire_iter(int n) {
-    int bits[32];  // tableau pour stocker les bits
-    int i = 0;     // compteur de bits
+    int bits[32];
+    int i = 0;
 
-    // Cas spécial : n = 0
     if (n == 0) {
         printf("0");
         return;
     }
 
-    // Remplissage du tableau
     while (n > 0) {
         bits[i++] = n % 2;
         n /= 2;
     }
 
-    // Affichage en ordre inverse
     for (int j = i - 1; j >= 0; j--) {
         printf("%d", bits[j]);
     }
